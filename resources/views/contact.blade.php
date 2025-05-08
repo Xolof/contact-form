47b3424f7b5d5d69e8cad1149f3c3bc19c6c4c79
Olof Johansson <olof@liberdev.se>
2025-05-08 08:02:57 +0200

    Style the contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -18,20 +18,20 @@
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
         <label for="name" class="sr-only">Name</label>
-        <input id="name" name="name" type="text" value="{{ old("name") }}" required class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-400 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm mb-4" placeholder="Full Name">
+        <input id="name" name="name" type="text" value="{{ old("name") }}" required class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mb-4 w-full" placeholder="Full Name">
       </div>
       <div>
         <label for="email" class="sr-only">Email address</label>
-        <input id="email" name="email" type="email" value="{{ old("email") }}" required class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm mb-4" placeholder="Email address">
+        <input id="email" name="email" type="email" value="{{ old("email") }}" required class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mb-4 w-full" placeholder="Email address">
       </div>
       <div>
         <label for="message" class="sr-only">Message</label>
-        <textarea id="message" name="message" required class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-400 rounded focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm" placeholder="Your message" rows="4">{{ old("message") }}</textarea>
+        <textarea id="message" name="message" required class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mb-4 w-full" placeholder="Your message" rows="4">{{ old("message") }}</textarea>
       </div>
     </div>
 
     <div>
-      <button type="submit" class="cursor-pointer group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+      <button type="submit" class="justify-center w-full inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
         Send Message
       </button>
     </div>
