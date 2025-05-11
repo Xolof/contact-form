3f4d71a4b73ba2c9281570639e1f52f75e14d3ba
Olof Johansson <olof@liberdev.se>
2025-05-11 17:05:21 +0200

    Decrease width of delete button for messages in admin interface

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,11 +25,11 @@
                             <p>{{ $message["created_at"] }}</p>
                             <p>{{ $message["sender_name"] }}</p>
                             <p>{{ $message["sender_email"] }}</p>
-                            <p>{{ $message["message"] }}</p>
+                            <p class="mb-2"> {{ $message["message"] }}</p>
                             <form method="POST" action="{{ route('delete-message') }}">
                             @csrf
                                 <input type="hidden" id="messageId" name="messageId" value="{{ $message["id"] }}"/>
-                                <input type="submit" id="delete" name="delete" value="Delete" class="cursor-pointer justify-center w-full inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-400 border border-transparent rounded-md font-semibold text-xs text-white dark:text-red-800 uppercase tracking-widest hover:bg-red-700 dark:hover:bg-white focus:bg-red-700 dark:focus:bg-white active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150"/>
+                                <input type="submit" id="delete" name="delete" value="Delete" class="cursor-pointer justify-center max-w-lg inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-400 border border-transparent rounded-md font-semibold text-xs text-white dark:text-red-800 uppercase tracking-widest hover:bg-red-700 dark:hover:bg-white focus:bg-red-700 dark:focus:bg-white active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150"/>
                             </form>
                         </div>
                     @endforeach
