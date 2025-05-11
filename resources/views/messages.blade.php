682df7fbf8a574f85783343b6c5333a285d24989
Olof Johansson <olof@liberdev.se>
2025-05-11 17:02:26 +0200

    Style the message cards

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,11 +1,11 @@
 <x-myNiceLayout>
   <x-slot:title>Messages</x-slot:title>
   @foreach($messages->all() as $message)
-    <div class="m-2 p-2 border-2 border-blue-300 rounded text-gray-200">
+    <div class="m-2 p-2 rounded bg-gray-900 text-gray-200 shadow-lg">
       <p>{{ $message["created_at"] }}</p>
       <p>Name: {{ $message["sender_name"] }}</p>
-      <p>Email: {{ $message["sender_email"] }}</p>
-      <p>Message {{ $message["message"] }}</p>
+      <p class="mb-2">Email: {{ $message["sender_email"] }}</p>
+      <p>{{ $message["message"] }}</p>
     </div>
   @endforeach
 </x-myNiceLayout>
