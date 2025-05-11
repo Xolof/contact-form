50ece7316efe22c3231f4f3f5305dc82278585a5
Olof Johansson <olof@liberdev.se>
2025-05-11 17:25:32 +0200

    remove vertical centering ofg content

--- resources/views/components/myNiceLayout.blade.php
+++ resources/views/components/myNiceLayout.blade.php
@@ -103,7 +103,7 @@
         </div>
     </nav>
 
-    <div class="min-h-screen flex items-center justify-center bg-gray-800 py-12 px-4 sm:px-6 lg:px-8">
+    <div class="min-h-screen flex justify-center bg-gray-800 py-12 px-4 sm:px-6 lg:px-8">
         <div class="max-w-md w-full space-y-8">
             <div>
                 <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-200">
