d6438d4190fa6693373b58deda7ee4d3f78dedca
Olof Johansson <olof@liberdev.se>
2025-05-11 16:37:06 +0200

    Show time for messages in guestbook

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -2,6 +2,7 @@
   <x-slot:title>Messages</x-slot:title>
   @foreach($messages->all() as $message)
     <div class="m-2 p-2 border-2 border-blue-300 rounded text-gray-200">
+      <p>{{ $message["created_at"] }}</p>
       <p>Name: {{ $message["sender_name"] }}</p>
       <p>Email: {{ $message["sender_email"] }}</p>
       <p>Message {{ $message["message"] }}</p>
