3be662aebf826db8c4f925bd9f1686b647d2fa40
Olof Johansson <olof@liberdev.se>
2025-05-13 13:02:30 +0200

    Show text 'there are no messages' if needed

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,5 +1,8 @@
 <x-myNiceLayout>
   <x-slot:title>Messages</x-slot:title>
+  @if(count($messages) === 0)
+    <p class="m-2 p-2 rounded text-gray-200 text-center">There are no published messages.</p>
+  @endif
   @foreach($messages->all() as $message)
     <div class="m-2 p-2 rounded bg-gray-900 text-gray-200 shadow-lg">
       <p>{{ $message["created_at"] }}</p>
